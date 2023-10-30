180847097a7cde6cc453d3c5a38a8c2960a75f27
Jauhien Lipko <nowmdr@protonmail.com>
2023-10-30 18:56:35 +0100

    feat: add styles and js for navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,27 @@
+export const Navigation = () => {
+  const mainClass = 'navigation';
+  const activeClass = '_active';
+  const navigation = document.querySelector(`.${mainClass}`);
+  if(!navigation) return;
+
+  const button = navigation.querySelector(`.${mainClass}__button`);
+  const list = navigation.querySelector(`.${mainClass}__list`);
+
+  function removeActiveClass() {
+    button.classList.remove(button.classList[0]+activeClass);
+    list.classList.remove(list.classList[0]+activeClass);
+  }
+
+  function toggleActiveClasses() {
+    button.classList.toggle(button.classList[0]+activeClass);
+    list.classList.toggle(list.classList[0]+activeClass);
+  }
+
+  button.addEventListener('click', toggleActiveClasses);
+
+  window.addEventListener('click', (e) => {
+    if (![...e.target.classList].some(i => i.includes(mainClass))) removeActiveClass();
+  });
+
+  console.log(navigation, list, button);
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import './index.scss';
 import { Component } from '../components/Component.js';
+import { Navigation } from '../components/Navigation';
 
 document.addEventListener("DOMContentLoaded", () => {
     Component();
+    Navigation();
 });
