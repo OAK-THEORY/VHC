f8a9ca64727da310f02a1bee5c26caf7616b4f9a
Jauhien Lipko <nowmdr@protonmail.com>
2023-11-02 11:27:11 +0100

    feat: add video formats to file loader

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
         exclude: '/node_modules/'
       },
       {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        test: /\.(png|mp4|webm|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource'
       },
       {
