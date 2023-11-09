62ebb14daf654ca3173f1379ba9b5e0d095a9c17
Jauhien Lipko <nowmdr@protonmail.com>
2023-11-09 14:15:49 +0100

    feat: add favicon, update utilities, change functionality for video

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,16 +1,39 @@
+import video1 from "../../video/wish-video.mp4"
+import video2 from "../../video/wish-video.webm"
+
 export const videoPlaceholder = () => {
   const section = document.querySelector('.wish');
   if (!section) return;
   const playButton = section.querySelector('.wish__play-button');
   const overlay = section.querySelector('.wish__video-overlay');
   const video = section.querySelector('.wish__video');
+  const userAgent = navigator.userAgent;
 
-  playButton.addEventListener('click', function () {
-    video.play();
-    overlay.classList.add('hidden');
+  function addListenerToPlayButton() {
+    playButton.addEventListener('click', function () {
+      video.play();
+      overlay.classList.add('hidden');
 
-    video.addEventListener('ended', function () {
-      overlay.classList.remove('hidden');
+      video.addEventListener('ended', function () {
+        overlay.classList.remove('hidden');
+      });
     });
-  });
+  }
+
+  function generateSourceForVideo(version) {
+    const source = document.createElement('source');
+    source.src = version;
+    source.type = version === video1 ? 'video/mp4' : 'video/webm';
+    video.appendChild(source);
+  }
+
+  if (video.canPlayType('video/mp4')) {
+    generateSourceForVideo(video1);
+    addListenerToPlayButton();
+  } else if (video.canPlayType('video/webm') && (userAgent.indexOf('Safari') === -1 && userAgent.indexOf('Edge') === -1)) {
+    generateSourceForVideo(video2);
+    addListenerToPlayButton();
+  } else {
+    playButton.classList.add('show-message');
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: { main: './src/pages/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-        publicPath: ''
+    publicPath: ''
   },
   mode: 'development',
   devServer: {
-    static: path.resolve(__dirname, './dist'), 
-    compress: true, 
-    port: 8080, 
+    static: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8080,
 
-    open: true 
+    open: true
   },
   module: {
-    rules: [ 
+    rules: [
       {
         test: /\.js$/,
         use: 'babel-loader',
         exclude: '/node_modules/'
       },
       {
-        test: /\.(png|webp|mp4|webm|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        test: /\.(ico|png|webp|mp4|webm|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource'
       },
       {
@@ -35,7 +35,7 @@ module.exports = {
           loader: 'css-loader',
           options: { importLoaders: 1 }
         },
-        'postcss-loader', 'sass-loader']
+          'postcss-loader', 'sass-loader']
       }
     ]
   },
@@ -45,5 +45,5 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
-  ] 
+  ]
 }
