2ae9b0a1ee5bb9c034c6fe6100cc0cb7443c421f
Nkanyiso <kndlovun@gmail.com>
2023-11-15 08:48:27 -0500

    create cloudfront

--- terraform/main-cloudfront.tf
+++ terraform/main-cloudfront.tf
@@ -1,68 +1,68 @@
-# locals {
-#   s3_origin_id = "vhc-s3-origin"
-# }
+locals {
+  s3_origin_id = "vhc-s3-origin"
+}
 
-# resource "aws_cloudfront_distribution" "s3_distribution" {
-#   origin {
-#     # domain_name              = "${aws_s3_bucket.vhc_s3_bucket.bucket}.s3-website-${var.region}.amazonaws.com"
-#     domain_name = "vhc-s3-bucket.s3-website-us-east-1.amazonaws.com"
-#     origin_id                = local.s3_origin_id
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    # domain_name              = "${aws_s3_bucket.vhc_s3_bucket.bucket}.s3-website-${var.region}.amazonaws.com"
+    domain_name = "vhc-s3-bucket.s3-website-us-east-1.amazonaws.com"
+    origin_id                = local.s3_origin_id
 
-#     # s3_origin_config {
-#     #   origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
-#     # }
-#   }
+    # s3_origin_config {
+    #   origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    # }
+  }
 
   
 
-#   enabled             = true
-#   is_ipv6_enabled     = true
-#   comment             = "VHC Cloudfront distribution"
-#   default_root_object = "index.html"
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "VHC Cloudfront distribution"
+  default_root_object = "index.html"
 
 
-# #   aliases = ["mysite.example.com", "yoursite.example.com"]
+#   aliases = ["mysite.example.com", "yoursite.example.com"]
 
-#   default_cache_behavior {
-#     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = local.s3_origin_id
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
 
-#     forwarded_values {
-#       query_string = false
+    forwarded_values {
+      query_string = false
 
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+      cookies {
+        forward = "none"
+      }
+    }
 
-#     viewer_protocol_policy = "redirect-to-https"
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#   }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
 
-#   price_class = "PriceClass_200"
+  price_class = "PriceClass_200"
 
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "whitelist"
-#       locations        = ["US", "CA", "GB", "DE", "PL"]
-#     }
-#   }
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE", "PL"]
+    }
+  }
 
-#   tags = {
-#     Environment = "PROD"
-#   }
+  tags = {
+    Environment = "PROD"
+  }
 
-#   viewer_certificate {
-#     cloudfront_default_certificate = true
-#   }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+# # CloudFront Origin Access Identity
+# resource "aws_cloudfront_origin_access_identity" "oai" {
+#   comment = "OAI for accessing private S3 content"
 # }
-
-# # # CloudFront Origin Access Identity
-# # resource "aws_cloudfront_origin_access_identity" "oai" {
-# #   comment = "OAI for accessing private S3 content"
-# # }
 
 
