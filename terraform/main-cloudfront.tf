c86b53f971dd13ad2a0a6e30f8d91d48fc756681
Nkanyiso <kndlovun@gmail.com>
2023-11-15 08:17:26 -0500

    removed other behaviour rules

--- terraform/main-cloudfront.tf
+++ terraform/main-cloudfront.tf
@@ -1,17 +1,10 @@
-# resource "aws_s3_bucket_acl" "vhc_s3_bucket_acl" {
-#   bucket = aws_s3_bucket.vhc_s3_bucket.id
-#   acl    = "private"
-# }
-
 locals {
-  s3_origin_id = "myS3Origin"
+  s3_origin_id = "vhc-s3-origin"
 }
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    # domain_name              = "${aws_s3_bucket.vhc_s3_bucket.bucket}.s3-website-${var.region}.amazonaws.com"
-    domain_name = aws_s3_bucket.vhc_s3_bucket.bucket_regional_domain_name
-    # origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    domain_name              = "${aws_s3_bucket.vhc_s3_bucket.bucket}.s3-website-${var.region}.amazonaws.com"
     origin_id                = local.s3_origin_id
 
     s3_origin_config {
@@ -24,13 +17,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   enabled             = true
   is_ipv6_enabled     = true
   comment             = "VHC Cloudfront distribution"
-  default_root_object = "src/index.html"
+  default_root_object = "index.html"
 
-#   logging_config {
-#     include_cookies = false
-#     bucket          = "vhc_s3_bucket.s3.amazonaws.com"
-#     prefix          = "myprefix"
-#   }
 
 #   aliases = ["mysite.example.com", "yoursite.example.com"]
 
@@ -47,55 +35,10 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       }
     }
 
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
-  }
-
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "/video/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  # Cache behavior with precedence 1
-  ordered_cache_behavior {
-    path_pattern     = "/images/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
   }
 
   price_class = "PriceClass_200"
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,3 @@
-output "website_domain" {
-  value = aws_s3_bucket.vhc_s3_bucket.website_domain
-  
-}
-
 output "website_endpoint" {
   value = aws_s3_bucket.vhc_s3_bucket.website_endpoint
   
