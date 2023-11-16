6e4b8757361c7c1447411c023e2293a1761feaec
Nkanyiso <kndlovun@gmail.com>
2023-11-16 13:19:31 -0500

    added DNS alias record

---
+++ terraform/main-route53.tf
@@ -0,0 +1,11 @@
+resource "aws_route53_record" "alias_record" {
+  zone_id = "Z0234196PUYV6WC2HAAI"
+  name    = "vanderbiltshop.com"
+  type    = "A"  # Use 'A' type for an Alias record
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
