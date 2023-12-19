3a7b61bef5926d82a0b02074792ac95d76f1905f
Jauhien Lipko <nowmdr@protonmail.com>
2023-12-19 15:42:32 +0100

    feat: add new banners with new promo

--- src/data/graduateHotel.js
+++ src/data/graduateHotel.js
@@ -11,49 +11,49 @@ export const graduateHotel = [
     title: "Vanderbilt Commodores Women’s Headliner Hooded Puffer Vest",
     subtitle: "Gameday couture",
     link: "https://www.fanatics.com/college/vanderbilt-commodores/-vanderbilt-commodores-gameday-couture-womens-headliner-hooded-puffer-vest-black/o-16+t-91709707+p-0244004699542+z-8-2797219800",
-    label: "25% off",
+    label: "-50% off",
     discountInfo: "Enter code",
-    discountCode: "25NOMIN",
-    discount: false
+    discountCode: "FREESHIP99",
+    discount: true
   },
   {
     imagePath: image5,
     title: "Vanderbilt Commodores Women’s Side Split Pullover",
     subtitle: "Gameday couture",
     link: "https://shop.vucommodores.com/womens-gameday-couture-cream-vanderbilt-commodores-side-split-logo-pullover-top/p-27783752849739+z-9246-683895410?_ref=p-DLP:m-GRID:i-r8c2:po-26",
-    label: "25% off",
+    label: "-50% off",
     discountInfo: "Enter code",
-    discountCode: "25NOMIN",
-    discount: false
+    discountCode: "FREESHIP99",
+    discount: true
   },
   {
     imagePath: image4,
     title: "Vanderbilt Commodores Men’s Waffle Knit Pullover Hoodie",
     subtitle: "League",
     link: "https://shop.vucommodores.com/mens-league-collegiate-wear-tan-vanderbilt-commodores-waffle-knit-pullover-hoodie/p-460088601635761626+z-91-836054217?_ref=p-DLP:m-GRID:i-r4c1:po-13",
-    label: "25% off",
+    label: "-50% off",
     discountInfo: "Enter code",
-    discountCode: "25NOMIN",
-    discount: false
+    discountCode: "FREESHIP99",
+    discount: true
   },
   {
     imagePath: image2,
     title: "Vanderbilt Commodores Women’s Black Guess Who's Back Long Sleeve T-Shirt",
     subtitle: "Gameday couture",
     link: "https://shop.vucommodores.com/womens-gameday-couture-black-vanderbilt-commodores-guess-whos-back-long-sleeve-t-shirt/p-460044689806421218+z-95-1851154301?_ref=p-DLP:m-GRID:i-r4c0:po-12",
-    label: "25% off",
+    label: "-50% off",
     discountInfo: "Enter code",
-    discountCode: "25NOMIN",
-    discount: false
+    discountCode: "FREESHIP99",
+    discount: true
   },
   {
     imagePath: image1,
     title: "Vanderbilt Commodores Men’s White/Silver Esteem Polo",
     subtitle: "Antigua",
     link: "https://shop.vucommodores.com/mens-antigua-white/silver-vanderbilt-commodores-esteem-polo/p-27550671111627+z-9619-2062307460?_ref=p-DLP:m-GRID:i-r10c2:po-32",
-    label: "25% off",
+    label: "-50% off",
     discountInfo: "Enter code",
-    discountCode: "25NOMIN",
-    discount: false
+    discountCode: "FREESHIP99",
+    discount: true
   }
 ];
