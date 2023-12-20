ea2c9e9e1a61777d7d2c84743e59a78a7734ce47
Jauhien Lipko <nowmdr@protonmail.com>
2023-12-20 13:25:58 +0100

    feat: remove "-" for promo label1

--- src/data/graduateHotel.js
+++ src/data/graduateHotel.js
@@ -11,7 +11,7 @@ export const graduateHotel = [
     title: "Vanderbilt Commodores Women’s Headliner Hooded Puffer Vest",
     subtitle: "Gameday couture",
     link: "https://www.fanatics.com/college/vanderbilt-commodores/-vanderbilt-commodores-gameday-couture-womens-headliner-hooded-puffer-vest-black/o-16+t-91709707+p-0244004699542+z-8-2797219800",
-    label: "-50% off",
+    label: "50% off",
     discountInfo: "Enter code",
     discountCode: "FREESHIP49",
     discount: true
@@ -21,7 +21,7 @@ export const graduateHotel = [
     title: "Vanderbilt Commodores Women’s Side Split Pullover",
     subtitle: "Gameday couture",
     link: "https://shop.vucommodores.com/womens-gameday-couture-cream-vanderbilt-commodores-side-split-logo-pullover-top/p-27783752849739+z-9246-683895410?_ref=p-DLP:m-GRID:i-r8c2:po-26",
-    label: "-50% off",
+    label: "50% off",
     discountInfo: "Enter code",
     discountCode: "FREESHIP49",
     discount: true
@@ -31,7 +31,7 @@ export const graduateHotel = [
     title: "Vanderbilt Commodores Men’s Waffle Knit Pullover Hoodie",
     subtitle: "League",
     link: "https://shop.vucommodores.com/mens-league-collegiate-wear-tan-vanderbilt-commodores-waffle-knit-pullover-hoodie/p-460088601635761626+z-91-836054217?_ref=p-DLP:m-GRID:i-r4c1:po-13",
-    label: "-50% off",
+    label: "50% off",
     discountInfo: "Enter code",
     discountCode: "FREESHIP49",
     discount: true
@@ -41,7 +41,7 @@ export const graduateHotel = [
     title: "Vanderbilt Commodores Women’s Black Guess Who's Back Long Sleeve T-Shirt",
     subtitle: "Gameday couture",
     link: "https://shop.vucommodores.com/womens-gameday-couture-black-vanderbilt-commodores-guess-whos-back-long-sleeve-t-shirt/p-460044689806421218+z-95-1851154301?_ref=p-DLP:m-GRID:i-r4c0:po-12",
-    label: "-50% off",
+    label: "50% off",
     discountInfo: "Enter code",
     discountCode: "FREESHIP49",
     discount: true
@@ -51,7 +51,7 @@ export const graduateHotel = [
     title: "Vanderbilt Commodores Men’s White/Silver Esteem Polo",
     subtitle: "Antigua",
     link: "https://shop.vucommodores.com/mens-antigua-white/silver-vanderbilt-commodores-esteem-polo/p-27550671111627+z-9619-2062307460?_ref=p-DLP:m-GRID:i-r10c2:po-32",
-    label: "-50% off",
+    label: "50% off",
     discountInfo: "Enter code",
     discountCode: "FREESHIP49",
     discount: true
